ba456d640317fb5471a0a701f95a6b6b244f825d
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-12 09:50:21 -0700

    Create reservation component

---
+++ src/Reservation/Reservation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Reservation = ({ details }) => {
+  return (
+    <article>
+      <h1>{details.name}</h1>
+
+    </article>
+    
+  )
+}
+
+export default Reservation;
