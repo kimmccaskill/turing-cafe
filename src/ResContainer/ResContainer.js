39a9473f9fde56e4ff43da1c968429afa80531fb
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-12 09:50:06 -0700

    Append reservations

--- src/ResContainer/ResContainer.js
+++ src/ResContainer/ResContainer.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Reservation from '../Reservation/Reservation.js'
 import './ResContainer.css';
 
-const ResContainer = () => {
-  
+const ResContainer = ({ reservations }) => {
+  const appendedReservations = reservations.map(reservation => {
+    return <Reservation details={reservation} />
+  });
   return (
-
+    <section>
+      {appendedReservations}
+    </section>
   )
 }
+
+export default ResContainer;
