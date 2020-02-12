3b427ee5c54f19af380adc4858f842db90371036
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-12 11:45:13 -0700

    Test app

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import App from './App';
 import { shallow } from 'enzyme';
+import { postRes } from '../apiCalls/apiCalls.js'
+
 
 describe('App', () => {
   let wrapper;
+  const mockPostRes = jest.fn();
+  const mockRes = {
+    id: Date.now(),
+    name: 'Kim',
+    date: '03/17',
+    time: '7:00',
+    number: '3',
+  }
   beforeEach(() => {
-    wrapper = shallow(<App />)
+    wrapper = shallow(<App postRes={mockPostRes}/>)
   })
 
   it('should be an instance of App component', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should call postRes with reservation', () => {
+    wrapper.instance().addReservation()
+
+    expect(wrapper.instance().postRes).toHaveBeenCalled()
+  })
 })
