a5396590c8c623efac495b6a9dbb30aa5f8a5e1a
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-12 09:50:36 -0700

    Change error message

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -2,7 +2,7 @@ export const getData = () => {
   return fetch('http://localhost:3001/api/v1/reservations')
   .then(response => {
       if(!response.ok) {
-           throw Error('Error while getting data!')
+           throw Error('Error while getting data. Sorry friends...')
       }
      return response.json()
   })
