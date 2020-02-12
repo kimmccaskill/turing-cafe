1ec0685b8a9f5b7d816a56eae236edb4ffaa45ef
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-12 09:27:23 -0700

    Replace hard code with components

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from '../Form/Form.js';
+import ResContainer from '../ResContainer/ResContainer.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-
-        </div>
-        <div className='resy-container'>
-          
-        </div>
+        <Form />
+        <ResContainer />
       </div>
     )
   }
