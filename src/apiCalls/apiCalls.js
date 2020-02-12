2acc2ba1ffec0e9cbda534e6ae42d465d92677fc
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-12 09:33:07 -0700

    Add fetch call for reservations

---
+++ src/apiCalls/apiCalls.js
@@ -0,0 +1,9 @@
+export const getData = () => {
+  return fetch('http://localhost:3001/api/v1/reservations')
+  .then(response => {
+      if(!response.ok) {
+           throw Error('Error while getting data!')
+      }
+     return response.json()
+  })
+}
