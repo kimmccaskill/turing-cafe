413642767308de059538fb8a275d80cd024593d1
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-12 10:03:05 -0700

    Add card details

--- src/Reservation/Reservation.js
+++ src/Reservation/Reservation.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Reservation.css'
 
 const Reservation = ({ details }) => {
   return (
-    <article>
+    <article className='res-card'>
       <h1>{details.name}</h1>
-
+      <p>{details.date}</p>
+      <p>{details.time} pm</p>
+      <p>Number of Guests: {details.number}</p>
+      <button className='cancel-btn'>Cancel</button>
     </article>
     
   )
